fc30cf0ad8855fc99a4bfd86520a001795643152
Lucas Sousa e Silva <sousaesilvalucas@gmail.com>
2020-11-19 14:17:58 -0300

    First Commit - Project Created

---
+++ src/test/java/site/sousaesilvalucas/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.sousaesilvalucas.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
