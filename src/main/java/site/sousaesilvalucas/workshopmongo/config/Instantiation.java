e5347b227a711769d3cec5181c8f948e92379711
Lucas Sousa e Silva <sousaesilvalucas@gmail.com>
2020-11-20 10:04:20 -0300

    Database Instantiation

---
+++ src/main/java/site/sousaesilvalucas/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package site.sousaesilvalucas.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import site.sousaesilvalucas.workshopmongo.domain.User;
+import site.sousaesilvalucas.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		// TODO Auto-generated method stub
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "BobGrey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
